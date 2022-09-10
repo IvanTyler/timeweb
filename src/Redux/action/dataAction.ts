535b3a24907120a2484d24f0bd9232afb566fbac
Ivan <Ivan_567.92@mail.ru>
2022-09-10 09:09:42 +0300

    delete types error

--- src/Redux/action/dataAction.ts
+++ src/Redux/action/dataAction.ts
@@ -1,21 +1,12 @@
-import { type } from "os";
 import { Dispatch } from "redux"
 import { data } from "../../data/mock"
-import { getDataActionType, getDataActionTypesEnum } from "../types/getDataTypes"
-
+import { GetDataAction, getDataActionTypesEnum } from "../types/getDataTypes"
 
 export const getDataFetch = () => {
-    return (dispath: Dispatch<getDataActionType>) => {
-        try {
-            dispath({
-                type: getDataActionTypesEnum.GET_DATA,
-                payload: data,
-            })
-        } catch (e) {
-            dispath({
-                type: getDataActionTypesEnum.GET_DATA_ERROR,
-                payload: 'Данных нет',
-            })
-        }
+    return (dispath: Dispatch<GetDataAction>) => {
+        dispath({
+            type: getDataActionTypesEnum.GET_DATA,
+            payload: data,
+        })
     }
 }
